e621aaef4d9ad59650c91ce69a29cd5e1c372eba
GuRuGu <peter.k.nets@gmail.com>
2021-05-17 13:30:54 +0300

    Show education levels on the map

--- main.js
+++ main.js
@@ -27,6 +27,10 @@ const chart = async () => {
     d3.json(countyDataUrl),
     d3.json(educationDataUrl),
   ]);
+  const landData = topojson.feature(
+    topology,
+    topology.objects.counties
+  ).features;
 
   //--> Min and max education values
   const [eduMin, eduMax] = d3.extent(edu, d => d.bachelorsOrHigher);
@@ -37,26 +41,31 @@ const chart = async () => {
     .domain(d3.range(eduMin, eduMax, (eduMax - eduMin) / 8))
     .range(d3.schemeReds[9]);
 
+  //--> Normalize land data
+  const landDataNormalized = landData.map(d => {
+    const result = edu.find(item => item.fips === d.id);
+
+    return {
+      ...d,
+      fill: colorScale(result.bachelorsOrHigher),
+      edu: result.bachelorsOrHigher,
+    };
+  });
+
+  console.log(landDataNormalized);
+
   //--> Display geo data
   svg
     .selectAll("path")
-    .data(topojson.feature(topology, topology.objects.counties).features)
+    .data(landDataNormalized)
     .enter()
     .append("path")
     .attr("d", path)
     .attr("stroke", "tomato")
-    .attr("fill", d => {
-      const result = edu.find(item => item.fips === d.id);
-
-      return colorScale(result.bachelorsOrHigher)[0];
-    })
+    .attr("fill", d => d.fill)
     .classed("county", true)
     .attr("data-fips", d => d.id)
-    .attr("data-education", d => {
-      const result = edu.find(item => item.fips === d.id);
-
-      return result.bachelorsOrHigher;
-    });
+    .attr("data-education", d => d.edu);
 
   //--> Legend
   const legendData = colorScale.range().map(d => {
